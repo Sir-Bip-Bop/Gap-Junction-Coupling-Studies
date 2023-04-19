8c91aff81677c251a9bea0792a70224befd1a55e
Sir-Bip-Bop <pablovizcainogarcia@gmail.com>
2023-04-19 20:22:12 +0100

    parallel code works

--- utils/firing_rate.py
+++ utils/firing_rate.py
@@ -4,4 +4,4 @@ import scipy as sp
 
 def compute_firing_rate(data,dt,t_final):
     spike_number=  (np.argwhere(np.array(data.todense())[0,:]>0) * dt).flatten()
-    return len(spike_number) / 2 * 1000 / t_final 
+    return len(spike_number) * 1000 / t_final 
