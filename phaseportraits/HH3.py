ca93f5fa4d1e7053862c8bc0cb8ff510a887bc36
Sir-Bip-Bop <pablovizcainogarcia@gmail.com>
2023-04-24 20:45:00 +0100

    Interseccionbetweenpoints found, check tomorrow

--- phaseportraits/HH3.py
+++ phaseportraits/HH3.py
@@ -14,7 +14,7 @@ def HH(V,h,*, m = 1, n = 1, I = 2, vt = -58):
 
 
 phase_diagram = PhasePortrait2D(HH, [[-80,80],[0,1]],
-	  dF_args = {'m': 0.2, 'n': 0.3, 'I': 2, 'vt': -58},
+	  dF_args = {'m': 0.5, 'n': 0.5, 'I': 2, 'vt': -58},
 	  MeshDim = 20,
 	  Title = 'HH Phase portrait (V-h)',
 	  xlabel = r'Voltage$(\mu V)$',
@@ -23,9 +23,9 @@ phase_diagram = PhasePortrait2D(HH, [[-80,80],[0,1]],
 )
 
 #phase_diagram.add_slider('C',valinit=1, valinterval=[0,2], valstep=0.2)
-phase_diagram.add_slider('m', valinit = 0.5, valinterval=[0,1],valstep=0.05)
-phase_diagram.add_slider('n', valinit=0.5, valinterval=[0,1],valstep=0.05)
-phase_diagram.add_slider('I', valinit = 2, valinterval=[0,4], valstep= 0.5)
+#phase_diagram.add_slider('m', valinit = 0.5, valinterval=[0,1],valstep=0.05)
+#phase_diagram.add_slider('n', valinit=0.5, valinterval=[0,1],valstep=0.05)
+#phase_diagram.add_slider('I', valinit = 2, valinterval=[0,4], valstep= 0.5)
 #phase_diagram.add_nullclines(xcolor='black', ycolor='green', yprecision=0.005,xprecision=0.05)
 
 
@@ -40,33 +40,33 @@ def HHy(z,V,*, m = 1, n = 1, I = 2, vt = -58):
 X = []
 Y = []
 I = []
-ii = np.linspace(0,1,100)
-bb = np.linspace(-80,80,100)
+ii = np.linspace(0,1,1000)
 
 for i in ii:
 	solve_x = sp.optimize.root_scalar(HHx,args=(i), x0 = -70, x1 = -50)
 	X.append(solve_x.root)
 
-for i in bb:
+X = np.array(X)
+
+for i in X:
 	solve_y = sp.optimize.root_scalar(HHy,args=(i), x0 = 0, x1 = 0.1)
 	Y.append(solve_y.root)
 
-X = np.array(X)
 Y = np.array(Y)
 
-dist = np.zeros((int(len(ii)),int(len(Y))))
-for i in range(len(ii)):
-    for j in range(len(Y)):
-              diff_y = ii[i] - Y[j]
-              diff_x = X[i] - bb[j]
-              dist[i,j] = abs(np.sqrt(diff_y*diff_y + diff_x/20*diff_x/20))
+#dist = np.zeros((int(len(ii)),int(len(Y))))
+#for i in range(len(ii)):
+#    for j in range(len(Y)):
+#              diff_y = ii[i] - Y[j]
+#              diff_x = X[i] - bb[j]
+#              dist[i,j] = abs(np.sqrt(diff_y*diff_y + diff_x/20*diff_x/20))
               
-idx = np.unravel_index(np.argmin(dist, axis=None), dist.shape)  
+#idx = np.unravel_index(np.argmin(dist, axis=None), dist.shape)  
+idx = np.argwhere(np.diff(np.sign(ii- Y))).flatten()
 
 phase_diagram.plot()
-phase_diagram.ax.plot(X,ii, color= 'red', marker= 'o' )
-phase_diagram.ax.plot(bb,Y, color = 'green', marker = 'o')
-phase_diagram.ax.plot(bb[idx[1]],Y[idx[1]], marker = 'o', markersize = 20,color = 'black')
-phase_diagram.ax.plot(X[idx[0]],ii[idx[0]], marker = 'o', markersize = 20,color = 'black')
+phase_diagram.ax.plot(X,ii, color= 'red')
+phase_diagram.ax.plot(X,Y, color = 'green')
+phase_diagram.ax.plot(X[idx],Y[idx], marker = 'o', markersize = 10,color = 'black')
 
 plt.show()
