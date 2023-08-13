f18d9cdd97d9f435c68a12881f97955323c3d0f0
Sir-Bip-Bop <pablovizcainogarcia@gmail.com>
2023-08-13 14:44:25 +0100

    phase portraits concluded?

--- phaseportraits/HH_V-m.py
+++ phaseportraits/HH_V-m.py
@@ -11,8 +11,8 @@ import scipy as sp
 plt.style.use('science')
 plt.rcParams["figure.figsize"] = (12,12)
 #plt.rcParams.update({"axes.grid" : True})
-plt.rcParams.update({"axes.titlesize": 25})
-plt.rcParams.update({"axes.labelsize": 23})
+plt.rcParams.update({"axes.titlesize": 27})
+plt.rcParams.update({"axes.labelsize": 25})
 
 
 
@@ -44,12 +44,17 @@ class my_function:
 
 
 #Loading the needed data from the .txt files 
-h_array = np.loadtxt('phaseportraits/h_data.txt')
+m_array = np.loadtxt('phaseportraits/m_data.txt')
 n_array = np.loadtxt('phaseportraits/n_data.txt')
+v_array = np.loadtxt('phaseportraits/v_data.txt')
+h_array = np.loadtxt('phaseportraits/h_data.txt')
 time = np.loadtxt('phaseportraits/time_data.txt')
 
+t_data = 66000
+t_slid = 66
+
 #Definition of the functions to integrate, that is the HH equations for the Voltage and the n variable
-def HHx(z, m, h = h_array[65000], n = n_array[65000], *, I = 2, vt = -58):
+def HHx(z, m, h = h_array[t_data], n = n_array[t_data], *, I = 2, vt = -58):
   V= z
   return  float(-30*m*m*m*h*(V-30) - 5*n*n*n*n*(V+90) - 0.1*(V+70) + I)
 
@@ -82,15 +87,25 @@ phase_diagram = PhasePortrait2D(ins, [[-80,80],[0,1]],
 	  color= 'cool',
 )
 
-phase_diagram.add_slider('t',valinit=65, valinterval=[0,time[1]], valstep=10)
+phase_diagram.add_slider('t',valinit=t_slid, valinterval=[0,time[1]], valstep=10)
 #phase_diagram.add_nullclines(xprecision=0.04, yprecision=0.06)
 fig, ax = phase_diagram.plot()
 ax.plot(X,ii, color= 'red', label = 'X - nullcine')
 ax.plot(bb,Y, color = 'green', label = 'Y - nullcline')
+ax.scatter(v_array[90000:120000],m_array[90000:120000],s=0.5)
+ax.scatter(v_array[t_data],m_array[t_data],s=40,color='green')
+custom_lines = [matplotlib.lines.Line2D([0], [0], color='red', lw=2),
+                matplotlib.lines.Line2D([0], [0], color='green', lw=2),
+                 matplotlib.lines.Line2D([0], [0], color='cyan', lw=2),
+                matplotlib.lines.Line2D([0], [0], color='blue', lw=2),
+                matplotlib.lines.Line2D([0], [0], marker='o', color='w', label='location',
+                      markerfacecolor='g', markersize=10, ls = ''),]
+
+ax.legend(custom_lines, ['V Peak', 'V Reset', 'Trajectory', 'Voltage - m evolution', 'Current Point'],loc='right', bbox_to_anchor=(0.9, 0.83),ncol=1, frameon=True, prop={'size': 12}, fancybox=True, shadow=False)
 circle = Trajectory2D(ins, n_points=10000, size=2, Range=[[-80 , 80], [0 , 1]],Fig = fig,Ax=ax,	  Title = ' ',
 	  xlabel = 'Voltage(mV)',
-	  ylabel = ' ')
-circle.initial_position(-59,0)
-circle.add_slider('t',valinit=65, valinterval=[0,time[1]], valstep=10)
+	  ylabel = 'Recovery Variable m')
+circle.initial_position(v_array[t_data],m_array[t_data])
+circle.add_slider('t',valinit=t_slid, valinterval=[0,time[1]], valstep=10)
 fig, ax2= circle.plot(color='cool')
 plt.show()
--- phaseportraits/HH_V-n.py
+++ phaseportraits/HH_V-n.py
@@ -11,8 +11,8 @@ import scipy as sp
 plt.style.use('science')
 plt.rcParams["figure.figsize"] = (12,12)
 #plt.rcParams.update({"axes.grid" : True})
-plt.rcParams.update({"axes.titlesize": 25})
-plt.rcParams.update({"axes.labelsize": 23})
+plt.rcParams.update({"axes.titlesize": 27})
+plt.rcParams.update({"axes.labelsize": 25})
 
 
 
@@ -45,12 +45,16 @@ class my_function:
 
 #Loading the needed data from the .txt files 
 m_array = np.loadtxt('phaseportraits/m_data.txt')
+n_array = np.loadtxt('phaseportraits/n_data.txt')
+v_array = np.loadtxt('phaseportraits/v_data.txt')
 h_array = np.loadtxt('phaseportraits/h_data.txt')
 time = np.loadtxt('phaseportraits/time_data.txt')
 
+t_data = 66000
+t_slid = 66
 
 #Definition of the functions to integrate, that is the HH equations for the Voltage and the n variable
-def HHx(z, n, m = m_array[65000], h = h_array[65000], *, I = 2, vt = -58):
+def HHx(z, n, m = m_array[t_data], h = h_array[t_data], *, I = 2, vt = -58):
   V= z
   return  float(-30*m*m*m*h*(V-30) - 5*n*n*n*n*(V+90) - 0.1*(V+70) + I)
 
@@ -85,17 +89,27 @@ phase_diagram = PhasePortrait2D(ins, [[-80,80],[0,1]],
 	  color= 'cool',
 )
 
-phase_diagram.add_slider('t',valinit=65, valinterval=[0,time[1]], valstep=10)
+phase_diagram.add_slider('t',valinit=t_slid, valinterval=[0,time[1]], valstep=10)
 #phase_diagram.add_nullclines( yprecision=0.001)
 
 fig, ax = phase_diagram.plot()
 ax.plot(X,ii, color= 'red', label = 'X - nullcine')
 ax.plot(bb,Y, color = 'green', label = 'Y - nullcline')
-circle = Trajectory2D(ins, n_points=10000, size=2, Range=[[-80 , 80], [0 , 1]],Fig = fig,Ax=ax,	  Title = 'HH Phase Portrait (V-n)',
-	  xlabel = ' ',
+ax.scatter(v_array[90000:120000],n_array[90000:120000],s=0.5)
+ax.scatter(v_array[t_data],n_array[t_data],s=40,color='green')
+custom_lines = [matplotlib.lines.Line2D([0], [0], color='red', lw=2),
+                matplotlib.lines.Line2D([0], [0], color='green', lw=2),
+                 matplotlib.lines.Line2D([0], [0], color='cyan', lw=2),
+                matplotlib.lines.Line2D([0], [0], color='blue', lw=2),
+                matplotlib.lines.Line2D([0], [0], marker='o', color='w', label='location',
+                      markerfacecolor='g', markersize=10, ls = ''),]
+
+ax.legend(custom_lines, ['V Peak', 'V Reset', 'Trajectory', 'Voltage - n evolution', 'Current Point'],loc='right', bbox_to_anchor=(0.9, 0.83),ncol=1, frameon=True, prop={'size': 12}, fancybox=True, shadow=False)
+circle = Trajectory2D(ins, n_points=10000, size=2, Range=[[-80 , 80], [0 , 1]],Fig = fig,Ax=ax,	  Title = ' ',
+	  xlabel = 'Voltage(mV)',
 	  ylabel = 'Recovery Variable')
-circle.initial_position(-60,0)
-circle.add_slider('t',valinit=65, valinterval=[0,time[1]], valstep=10)
+circle.initial_position(v_array[t_data],n_array[t_data])
+circle.add_slider('t',valinit=t_slid, valinterval=[0,time[1]], valstep=10)
 fig, ax2= circle.plot(color='cool')
 
 plt.show()
